5952a5d5babf59fb4838dcae3362649fddd12c90
alex <alex.hmn18@gmail.com>
2025-04-04 11:58:16 +0200

    feat: modification du fichier route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './views/user/user.component';
+import { AuthComponent } from './views/user/auth/auth.component';
+import { LoginComponent } from './views/user/login/login.component';
+import { RegisterComponent } from './views/user/register/register.component';
+import { HomeComponent } from './views/user/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: UserComponent,
+        children: [
+            { path: '', component: AuthComponent },
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: RegisterComponent },
+            { path: 'home', component: HomeComponent },
+        ]
+    },
+
+
+    // TODO : Page 404
+    {path:'**', redirectTo:'/' },
+];
