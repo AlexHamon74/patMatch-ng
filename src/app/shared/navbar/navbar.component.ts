dc469230de1b24b00a3bc27730c2379b9860d1d1
alex <alex.hmn18@gmail.com>
2025-05-26 09:58:57 +0200

    feat: affichage lien navbar selon role

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UserService } from '../../core/services/user.service';
 
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+    selector: 'app-navbar',
+    standalone: true,
+    imports: [RouterLink],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+    //Injection du service UserService
+    userService = inject(UserService);
+
+    //Méthode pour savoir si le user est un client
+    isClient() {
+        return this.userService.hasRole('ROLE_CLIENT');
+    };
+
+    //Méthode pour savoir si le user est un éleveur
+    isBreeder() {
+        return this.userService.hasRole('ROLE_ELEVEUR');
+    };
 
 }
